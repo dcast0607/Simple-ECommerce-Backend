15308390add8d38375eabfdb9c972419d66c4829
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 16:33:46 -0700

    Commenting on ./config/connection.js file

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
+// This package is required to be able to use environment variables
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
+// If the application is hosted through a web service provider our 
+// app will connect to a web hosted version of our database, otherwise
+// we will connect to a local version of our database.
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
