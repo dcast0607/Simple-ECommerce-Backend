0f4f38c7abccf4735515ba9873d4e4abc8db19c3
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 17:21:27 -0700

    Pushing model associations and comments

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,43 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// In this file we are creating relationships/associations between the
+// different models. I heavily used this resource to help figure out
+// how these models need to be associated with each other. 
+
+// Resource: https://sequelize.org/docs/v6/core-concepts/assocs/
+
+/*
+  Requested Associations: 
+
+    Product belongs to Category, and Category has many Product models, as a category can have multiple products but a product can only belong to one category.
+
+    Product belongs to many Tag models, and Tag belongs to many Product models. Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
+
+*/
+
 // Products belongsTo Category
+ProductTag.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsTo(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
