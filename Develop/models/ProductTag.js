15d577978c3a34e88854dc720f02ef0218700674
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 17:07:26 -0700

    Adding comments to ./models/ProductTags.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -4,6 +4,10 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+
+// Similar to the "Category" and "Product" models, we start off by
+// declaring a few key properties for this model. The big takeaway here
+// is that this model will be referencing the "product" and "tag" models.
 ProductTag.init(
   {
     id: {
